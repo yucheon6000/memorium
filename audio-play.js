1acaba8946da6790dd778058e943fb91adbb9422
yucheon6000 <yucheon6000@gmail.com>
2024-07-12 12:49:13 +0900

    Remove some audio files

--- audio-play.js
+++ audio-play.js
@@ -1,44 +1,36 @@
 
 const audioList = [
     {
-        "file-name": "0.m4a",
-        "content": "우리 지훈이 결혼했나? 혼자 왔니?"
-    },
-    {
-        "file-name": "1.m4a",
+        "file-name": "./audios/1.m4a",
         "content": "기억해줘서 고마워요."
     },
     {
-        "file-name": "2.m4a",
+        "file-name": "./audios/2.m4a",
         "content": "오랜만이네요. 이렇게 이야기하게 되어서 정말 감사합니다!"
     },
     {
-        "file-name": "3.m4a",
+        "file-name": "./audios/3.m4a",
         "content": "항상 먼 길 찾아 와주셔서 정말 고마워요."
     },
     {
-        "file-name": "4.m4a",
+        "file-name": "./audios/4.m4a",
         "content": "헬로 나이스 투 미 츄~"
     },
     {
-        "file-name": "5.m4a",
+        "file-name": "./audios/5.m4a",
         "content": "덕분에 행복하게 생을 마감하고 있습니다. 부디 그 축복받은 남은 삶 행복하게 보내시길 바래요."
     },
     {
-        "file-name": "6.m4a",
-        "content": "헤이~ 왓 업 베이비~, 컴 온 베이비!"
-    },
-    {
-        "file-name": "7.m4a",
-        "content": "저도 재밌게 녹음하고 있거든요. 그니까 저 없다고 울지 그러시지 말고"
+        "file-name": "./audios/7.m4a",
+        "content": "저도 재밌게 녹음하고 있거든요. 그러니까 저 없다고 울고 그러시지 말고."
     }
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-    let audioIdx = Math.floor(Math.random() * 8);
+    let audioIdx = Math.floor(Math.random() * 6);
 
     let playBtn = document.querySelector("#play-btn");
-    var audio = new Audio(`./audios/${audioIdx}.m4a`);
+    var audio = new Audio(audioList[audioIdx]["file-name"]);
     audio.addEventListener("ended", () => {
         setPlayIcon();
         isPlaying = false;
@@ -65,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let dwlBtn = document.querySelector("#download-btn");
     dwlBtn.addEventListener("click", () => {
-        clickHandler(`./audios/${audioIdx}.m4a`, "고 천경자씨가 전하는 인사말.m4a");
+        clickHandler(audioList[audioIdx]["file-name"], "고 천경자씨가 전하는 인사말.m4a");
     });
 });
 
